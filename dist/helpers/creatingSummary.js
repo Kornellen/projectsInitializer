67726e474831e75b73a7edd0950bc60cc5a64642
kornellen <kornel.konopka@onet.pl>
2024-12-29 00:20:01 +0100

    Changed TypeScript Compiler | v2.0.0

--- dist/helpers/creatingSummary.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = projectSummary;
-function projectSummary(projectInfos, projectType) {
-    const dateOfCreation = new Date().toLocaleTimeString();
-    return console.log(`Project created at ${projectInfos.projectPath} | Time: ${dateOfCreation} | Project Type: ${projectType} | Project Name: ${projectInfos.projectName} | Good Luck!`
-        .bgBlue);
-}
